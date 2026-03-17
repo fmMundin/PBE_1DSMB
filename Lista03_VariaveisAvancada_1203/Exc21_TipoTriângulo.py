7a1c35bebc8b692f4add7389bebbd1e68f4ae935
FmMundin <fmundin@alu.paulinia.sp.gov.br>
2026-03-17 09:09:20 -0300

    Adição do exc21 finalizado

--- Lista03_VariaveisAvancada_1203/Exc21_TipoTriângulo.py
+++ Lista03_VariaveisAvancada_1203/Exc21_TipoTriângulo.py
@@ -1,8 +1,14 @@
+import math
+
 a = int(input("Informe o lado A do triângulo: "))
 b = int(input("Informe o lado B do triângulo: "))
 c = int(input("Informe o lado C do triângulo: "))
 
 print("O triângulo é...\n")
-print(f"Isósceles: {a == b and a != c or a != b and a == c} ")
-print(f"Escaleno: {a != b and a != c }")
+print(f"Isósceles: {a == b and a != c or a != b and a == c or b == c and a != c} ")
+print(f"Escaleno: {a != b and a != c and b != c } ")
 print(f"Equilátero: {a == b and a == c and b == c}")
+
+perimetro = (a + b + c) / 2
+area = math.sqrt(perimetro*(perimetro-a)*(perimetro-b)*(perimetro -c))
+print(f"Area: {area} ")
